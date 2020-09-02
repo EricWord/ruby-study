eb7184abfd7af15517441f91607dda4b29214849
“cuigs” <cuiguangsong@miliantech.com>
2020-09-02 16:25:39 +0800

    unless的用法

--- test/Test3.rb
+++ test/Test3.rb
@@ -8,5 +8,19 @@
 # end
 
 
-$debug = 1
-print "debug\n" if $debug
+# $debug = 1
+# print "debug\n" if $debug
+#
+# x = 1
+# unless x > 2
+#   puts "x 小于等于 2"
+# else
+#   puts "x 大于 2"
+# end
+#
+$var = 1
+print "1 -- 这一行输出\n" if $var
+print "2 -- 这一行不输出" unless $var
+$var = false
+
+print "3 -- 这一行输出\n" unless $var
